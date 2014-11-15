1e1d528988aae6405619b81f5bc81e54328673db
viator75 <tough@fontech.com.tw>
2014-11-15 17:10:50 +0800

    gix

--- config/routes.rb
+++
@@ -1,7 +0,0 @@
-Rails.application.routes.draw do
-  get 'about' => 'main#about'
-  get 'service' => 'main#service'
-  get 'product' => 'main#product'
-  get 'contact' => 'main#contact'
-  root 'main#index'
-end
